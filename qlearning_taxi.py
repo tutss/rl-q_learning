2f7d5b08de74be5df9aa6a90604476cf72c388e9
tutss <artur_santos@usp.br>
2020-08-18 19:57:33 -0300

    Added some modifications + requirements

--- qlearning_taxi.py
+++ qlearning_taxi.py
@@ -1,9 +1,18 @@
+import sys
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 style.use('ggplot')
+
+# parameters
+lr = 0.1
+gamma = 0.9
+episodes = 100_000
+show_every = 10_000
+epsilon = 0.7
+epsilon_decay_value = 0.9998
 
 def plot_metrics():
     plt.figure(figsize=(15,10))
@@ -15,31 +24,27 @@ def plot_metrics():
     plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['eps'], label='eps')
     plt.show()
 
+def q_learning(q_table, state_action, new_state, reward):
+    max_q = np.max(q_table[new_state]) if new_state != -1 else 0
+    current_q = q_table[state_action]
+    new_q = (1-lr) * current_q + lr * (reward + gamma * max_q)
+    q_table[state_action] = new_q
+
 env = gym.make('Taxi-v2')
 
-# parameters
-lr = 0.1
-gamma = 0.9
-episodes = 100_000
-show_every = 10_000
-
-epsilon = 0.7
-epsilon_decay_value = 0.9998
-
-print(epsilon_decay_value)
-
+print("Epsilon decay value = {}".format(epsilon_decay_value))
 # env info
-print(env.observation_space)
-print(env.action_space)
-print(env.action_space.n)
+print("Observation space = {}".format(env.observation_space))
+print("Action space = {}".format(env.action_space))
+print("Number of available actions = {}".format(env.action_space.n))
 
 # discrete values
 size = [500]
 
 # creating q table
-q_table = np.random.uniform(low=-10, high=20, size=(size + [env.action_space.n]))
+q_table = np.zeros((size + [env.action_space.n]))
 
-print(q_table.shape)
+print("Q-Table shape = {}".format(q_table.shape))
 
 ep_rewards = [] # ep rewards
 aggr_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max': [], 'eps': []}
@@ -49,48 +54,35 @@ epsilons = list()
 
 for ep in range(1, episodes):
     epsilons.append(epsilon)
-
     ep_reward = 0
-
+    
     render = False
     if ep % show_every == 0:
         print("===> Episode {}".format(ep))
         # render = True
-
+    
     # initial state
-    discrete_state = env.reset()
-
-    # frames
-    frames = []
-
+    state = env.reset()
     while True:
         # acting greedy sometimes (exploration)
         if np.random.random() > epsilon:
-            action = np.argmax(q_table[discrete_state])
+            action = np.argmax(q_table[state])
         else:
             action = env.action_space.sample()
 
         new_state, reward, done, _ = env.step(action)
-        new_discrete_state = new_state
         ep_reward += reward
-
-        state_action = (discrete_state, action)
-        
-        discrete_state = new_discrete_state
-
+        state_action = (state, action)
+        state = new_state
         if render: 
             env.render()
         if not done:
-            max_future_q = np.max(q_table[new_discrete_state]) # max value for the new state at t+1
-            current_q = q_table[state_action] # getting current q value at t
-            new_q = (1-lr) * current_q + lr * (reward + gamma * max_future_q) # q learning formula
-            q_table[state_action] = new_q # update state-action at t with new q
+            q_learning(q_table, state_action, new_state, reward)
         else: #new_state[0] >= env.goal_position: # reached terminal state
-            q_table[state_action] = 0 # given that we initialize q_table with random variables
+            q_learning(q_table, state_action, -1, reward)
             break
 
-    if end_epsilon_decay >= ep >= start_epsilon_decay:
-        epsilon *= epsilon_decay_value
+    epsilon *= epsilon_decay_value
     
     ep_rewards.append(ep_reward)
     if not ep % show_every:
